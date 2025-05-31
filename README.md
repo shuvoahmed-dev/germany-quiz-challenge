c03be0d7643d916b9be0a6ab1363f11449392b5c
shuvoahmed-dev <shuvoahmed.work@gmail.com>
2025-05-31 14:33:19 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# germany-quiz-challenge
+A simple Python console quiz game that tests your knowledge about Germany’s geography, culture, history, and football.
---
+++ quiz_challenge.py
@@ -0,0 +1,147 @@
+questions = [
+    ["What is the capital city of Germany?", "Munich", "Frankfurt", "Berlin", "Hamburg", "Stuttgart", 3],
+    ["Which river flows through the city of Cologne?", "Elbe", "Weser", "Rhine", "Danube", "Main", 3],
+    ["In which German city is Oktoberfest held annually?", "Berlin", "Hamburg", "Munich", "Cologne", "Dresden", 3],
+    ["What is the official language spoken in Germany?", "German", "Dutch", "English", "French", "Danish", 1],
+    ["Germany is part of which continent?", "Asia", "South America", "Europe", "Africa", "North America", 3],
+    ["What is the currency used in Germany?", "Euro", "Dollar", "Franc", "Pound", "Lira", 1],
+    ["Which German car manufacturer produces the BMW brand?", "Volkswagen", "Mercedes", "BMW", "Audi", "Porsche", 3],
+    ["Which German city is famous for its Gothic cathedral?", "Cologne", "Leipzig", "Nuremberg", "Bremen", "Stuttgart", 1],
+    ["Who was the Chancellor of Germany during reunification in 1990?", "Adenauer", "Helmut Kohl", "Gerhard Schröder", "Angela Merkel", "Willy Brandt", 2],
+    ["Which sea is located to the north of Germany?", "Red Sea", "Baltic Sea", "North Sea", "Black Sea", "Caspian Sea", 3],
+    ["What are the colors of the German national flag?", "Red, Green, Yellow", "Red, Black, Yellow", "Blue, White, Red", "Black, Yellow, White", "Black, Red, Gold", 5],
+    ["What is the largest federal state in Germany by area?", "Bavaria", "Hesse", "Saxony", "Berlin", "Saarland", 1],
+    ["Which German city is known as a financial hub?", "Berlin", "Hamburg", "Frankfurt", "Munich", "Bonn", 3],
+    ["Which German philosopher wrote 'Thus Spoke Zarathustra'?", "Karl Marx", "Immanuel Kant", "Friedrich Nietzsche", "Martin Heidegger", "Georg Hegel", 3],
+    ["Which German scientist developed the theory of relativity?", "Albert Einstein", "Carl Gauss", "Alexander von Humboldt", "Robert Koch", "Max Planck", 1],
+    ["Which country borders Germany to the west?", "Poland", "Austria", "France", "Czech Republic", "Switzerland", 3],
+    ["What is considered Germany’s national dish?", "Schnitzel", "Bratwurst", "Sauerbraten", "Currywurst", "Rouladen", 3],
+    ["Who collected many famous fairy tales in Germany?", "Hans Christian Andersen", "Charles Perrault", "Brothers Grimm", "J.K. Rowling", "J.R.R. Tolkien", 3],
+    ["What is the capital of Bavaria?", "Hamburg", "Cologne", "Munich", "Berlin", "Stuttgart", 3],
+    ["In which year did the Berlin Wall fall?", "1985", "1987", "1989", "1990", "1991", 3],
+    ["Which German football club has won the most Bundesliga titles?", "Borussia Dortmund", "Bayern Munich", "RB Leipzig", "Schalke 04", "Bayer Leverkusen", 2],
+    ["What are the colors of the German national football team?", "Red", "Black", "White and Black", "Blue", "Green", 3],
+    ["How many times has Germany won the FIFA World Cup?", "2", "3", "4", "5", "6", 3],
+    ["Who scored the winning goal for Germany in the 2014 World Cup final?", "Thomas Müller", "Mario Götze", "Toni Kroos", "Miroslav Klose", "Philipp Lahm", 2],
+    ["Which German football club plays its home games at Signal Iduna Park?", "Bayern Munich", "Borussia Dortmund", "Bayer Leverkusen", "VfL Wolfsburg", "Schalke 04", 2],
+    ["What is the capital city of Germany?", "Munich", "Frankfurt", "Berlin", "Hamburg", "Stuttgart", 3],
+    ["Which river flows through the city of Cologne?", "Elbe", "Weser", "Rhine", "Danube", "Main", 3],
+    ["In which German city is Oktoberfest held annually?", "Berlin", "Hamburg", "Munich", "Cologne", "Dresden", 3],
+    ["What is the official language spoken in Germany?", "German", "Dutch", "English", "French", "Danish", 1],
+    ["Germany is part of which continent?", "Asia", "South America", "Europe", "Africa", "North America", 3],
+    ["What is the currency used in Germany?", "Euro", "Dollar", "Franc", "Pound", "Lira", 1],
+    ["Which German car manufacturer produces the BMW brand?", "Volkswagen", "Mercedes", "BMW", "Audi", "Porsche", 3],
+    ["Which German city is famous for its Gothic cathedral?", "Cologne", "Leipzig", "Nuremberg", "Bremen", "Stuttgart", 1],
+    ["Who was the Chancellor of Germany during reunification in 1990?", "Adenauer", "Helmut Kohl", "Gerhard Schröder", "Angela Merkel", "Willy Brandt", 2],
+    ["Which sea is located to the north of Germany?", "Red Sea", "Baltic Sea", "North Sea", "Black Sea", "Caspian Sea", 3],
+    ["What are the colors of the German national flag?", "Red, Green, Yellow", "Red, Black, Yellow", "Blue, White, Red", "Black, Yellow, White", "Black, Red, Gold", 5],
+    ["What is the largest federal state in Germany by area?", "Bavaria", "Hesse", "Saxony", "Berlin", "Saarland", 1],
+    ["Which German city is known as a financial hub?", "Berlin", "Hamburg", "Frankfurt", "Munich", "Bonn", 3],
+    ["Which German philosopher wrote 'Thus Spoke Zarathustra'?", "Karl Marx", "Immanuel Kant", "Friedrich Nietzsche", "Martin Heidegger", "Georg Hegel", 3],
+    ["Which German scientist developed the theory of relativity?", "Albert Einstein", "Carl Gauss", "Alexander von Humboldt", "Robert Koch", "Max Planck", 1],
+    ["Which country borders Germany to the west?", "Poland", "Austria", "France", "Czech Republic", "Switzerland", 3],
+    ["What is considered Germany’s national dish?", "Schnitzel", "Bratwurst", "Sauerbraten", "Currywurst", "Rouladen", 3],
+    ["Who collected many famous fairy tales in Germany?", "Hans Christian Andersen", "Charles Perrault", "Brothers Grimm", "J.K. Rowling", "J.R.R. Tolkien", 3],
+    ["What is the capital of Bavaria?", "Hamburg", "Cologne", "Munich", "Berlin", "Stuttgart", 3],
+    ["In which year did the Berlin Wall fall?", "1985", "1987", "1989", "1990", "1991", 3],
+    ["Which German football club has won the most Bundesliga titles?", "Borussia Dortmund", "Bayern Munich", "RB Leipzig", "Schalke 04", "Bayer Leverkusen", 2],
+    ["What are the colors of the German national football team?", "Red", "Black", "White and Black", "Blue", "Green", 3],
+    ["How many times has Germany won the FIFA World Cup?", "2", "3", "4", "5", "6", 3],
+    ["Who scored the winning goal for Germany in the 2014 World Cup final?", "Thomas Müller", "Mario Götze", "Toni Kroos", "Miroslav Klose", "Philipp Lahm", 2],
+    ["Which German football club plays its home games at Signal Iduna Park?", "Bayern Munich", "Borussia Dortmund", "Bayer Leverkusen", "VfL Wolfsburg", "Schalke 04", 2],
+    ["Which German city was divided into East and West during the Cold War?", "Munich", "Cologne", "Berlin", "Frankfurt", "Dresden", 3],
+    ["What year did Germany officially reunify?", "1987", "1989", "1990", "1991", "1993", 3],
+    ["Who was the first President of the Federal Republic of Germany?", "Theodor Heuss", "Konrad Adenauer", "Richard von Weizsäcker", "Gustav Heinemann", "Walter Scheel", 1],
+    ["Which German beer purity law was enacted in 1516?", "Reinheitsgebot", "Biergesetz", "Brauordnung", "Gesetz der Hopfen", "Malzgesetz", 1],
+    ["Which German city hosts the famous trade fair called Messe?", "Frankfurt", "Hamburg", "Berlin", "Düsseldorf", "Stuttgart", 1],
+    ["What is the German word for the capital city of Germany?", "Hauptstadt", "Stadt", "Metropole", "Bundesstadt", "Landeshauptstadt", 1],
+    ["Which German club won the UEFA Champions League in 2013?", "Borussia Dortmund", "Bayern Munich", "Schalke 04", "RB Leipzig", "Werder Bremen", 2],
+    ["Who is Germany’s all-time top scorer in the Bundesliga?", "Gerd Müller", "Miroslav Klose", "Robert Lewandowski", "Claudio Pizarro", "Jupp Heynckes", 1],
+    ["Which famous castle inspired Disney's Sleeping Beauty Castle?", "Neuschwanstein Castle", "Hohenzollern Castle", "Eltz Castle", "Heidelberg Castle", "Wartburg Castle", 1],
+    ["Which German car brand is known for the slogan ‘Vorsprung durch Technik’?", "BMW", "Audi", "Mercedes-Benz", "Volkswagen", "Porsche", 2],
+    ["Which city is the economic center of Germany?", "Hamburg", "Frankfurt", "Berlin", "Munich", "Cologne", 2],
+    ["Which German football team won the first Bundesliga title in 1963?", "1. FC Köln", "Borussia Dortmund", "Bayern Munich", "Hamburger SV", "Eintracht Frankfurt", 1],
+    ["Which famous German film director made 'Metropolis'?", "Werner Herzog", "Fritz Lang", "Rainer Werner Fassbinder", "Tom Tykwer", "F.W. Murnau", 2],
+    ["What is the tallest mountain in Germany?", "Zugspitze", "Brocken", "Watzmann", "Feldberg", "Großer Arber", 1],
+    ["Which German city is famous for its port, one of the largest in Europe?", "Hamburg", "Bremen", "Kiel", "Lübeck", "Rostock", 1],
+    ["Who wrote the German national anthem’s lyrics?", "August Heinrich Hoffmann von Fallersleben", "Johann Wolfgang von Goethe", "Friedrich Schiller", "Heinrich Heine", "Bertolt Brecht", 1],
+    ["Which German footballer was captain of the 2014 World Cup-winning team?", "Philipp Lahm", "Bastian Schweinsteiger", "Manuel Neuer", "Mesut Özil", "Mats Hummels", 1],
+    ["Which German philosopher wrote 'Critique of Pure Reason'?", "Immanuel Kant", "Georg Hegel", "Arthur Schopenhauer", "Friedrich Nietzsche", "Karl Marx", 1],
+    ["In which German city is the famous museum ‘Pergamonmuseum’ located?", "Berlin", "Munich", "Hamburg", "Cologne", "Frankfurt", 1],
+    ["Which German state is known for the Rhine Valley and its castles?", "Rhineland-Palatinate", "Baden-Württemberg", "Hesse", "Saxony", "Thuringia", 1],
+    ["Which German football club is nicknamed 'Die Schwarzgelben' (The Black and Yellows)?", "Borussia Dortmund", "Bayern Munich", "Schalke 04", "RB Leipzig", "Hamburger SV", 1],
+    ["Which German automobile company was founded by Karl Benz?", "Volkswagen", "BMW", "Mercedes-Benz", "Audi", "Porsche", 3],
+    ["Which city hosts the Frankfurt Book Fair, one of the largest in the world?", "Frankfurt", "Berlin", "Leipzig", "Düsseldorf", "Munich", 1],
+    ["Which German composer is famous for his Brandenburg Concertos?", "Johann Sebastian Bach", "Ludwig van Beethoven", "Richard Wagner", "Johannes Brahms", "Felix Mendelssohn", 1],
+    ["Which German football club’s home stadium is the Allianz Arena?", "Bayern Munich", "Borussia Dortmund", "Bayer Leverkusen", "Schalke 04", "Hamburg SV", 1],
+    ["Which treaty ended World War I and imposed heavy reparations on Germany?", "Treaty of Versailles", "Treaty of Paris", "Treaty of Tordesillas", "Treaty of Westphalia", "Treaty of Utrecht", 1],
+    ["Who was Chancellor of Germany during the reunification?", "Helmut Kohl", "Gerhard Schröder", "Angela Merkel", "Konrad Adenauer", "Willy Brandt", 1],
+    ["Which German city was heavily bombed during World War II and later rebuilt?", "Dresden", "Cologne", "Munich", "Berlin", "Hamburg", 1],
+    ["Which German philosopher wrote 'Thus Spoke Zarathustra'?", "Friedrich Nietzsche", "Immanuel Kant", "Georg Hegel", "Karl Marx", "Arthur Schopenhauer", 1],
+    ["Which football player scored the winning goal in the 2014 World Cup final?", "Mario Götze", "Thomas Müller", "Miroslav Klose", "André Schürrle", "Mesut Özil", 1],
+    ["Which German football team is the record holder for most Bundesliga titles?", "Bayern Munich", "Borussia Dortmund", "Hamburger SV", "Schalke 04", "Werder Bremen", 1],
+    ["What is the name of the famous German automobile race held annually?", "Nürburgring", "Le Mans", "Monza", "Silverstone", "Spa-Francorchamps", 1],
+    ["Which German city is known for its vibrant carnival celebrations called 'Karneval'?", "Cologne", "Munich", "Berlin", "Düsseldorf", "Frankfurt", 1],
+    ["What year was the Berlin Wall built?", "1961", "1949", "1953", "1971", "1989", 1],
+    ["Which German footballer is known as 'Der Kaiser'?", "Franz Beckenbauer", "Gerd Müller", "Lothar Matthäus", "Miroslav Klose", "Jürgen Klinsmann", 1],
+    ["What was the name of the ancient Germanic tribe defeated by the Romans in the Battle of the Teutoburg Forest?", "Cherusci", "Goths", "Vandals", "Franks", "Saxons", 1],
+    ["Which German author wrote 'The Metamorphosis'?", "Franz Kafka", "Thomas Mann", "Hermann Hesse", "Heinrich Böll", "Günter Grass", 1],
+    ["What is the largest beer festival in the world, held annually in Munich?", "Oktoberfest", "Cannstatter Volksfest", "Starkbierfest", "Frühlingsfest", "Karneval", 1],
+    ["Which German city is home to the oldest university in the country, founded in 1386?", "Heidelberg", "Leipzig", "Berlin", "Munich", "Tübingen", 1],
+    ["Which German football club was the runner-up in the 1997 UEFA Champions League final?", "Borussia Dortmund", "Bayern Munich", "Schalke 04", "Hamburger SV", "Werder Bremen", 1],
+    ["Which German composer created the famous 'Ride of the Valkyries'?", "Richard Wagner", "Ludwig van Beethoven", "Johann Sebastian Bach", "Felix Mendelssohn", "Robert Schumann", 1],
+    ["What is the German term for a federal state?", "Bundesland", "Landkreis", "Stadtteil", "Bezirk", "Kreis", 1],
+    ["Which German city is famous for its red-light district called the 'Reeperbahn'?", "Hamburg", "Berlin", "Cologne", "Frankfurt", "Düsseldorf", 1],
+    ["Who was the German president before Joachim Gauck?", "Christian Wulff", "Horst Köhler", "Joachim Gauck", "Frank-Walter Steinmeier", "Gustav Heinemann", 1],
+    ["What is the name of the German car company founded in 1937 in Wolfsburg?", "Volkswagen", "BMW", "Audi", "Mercedes-Benz", "Opel", 1],
+    ["Which German footballer has the most caps (appearances) for the national team?", "Lothar Matthäus", "Miroslav Klose", "Philipp Lahm", "Bastian Schweinsteiger", "Jürgen Klinsmann", 1],
+    ["Which German city was the capital of East Germany (GDR)?", "East Berlin", "Leipzig", "Dresden", "Magdeburg", "Erfurt", 1],
+    ["Which German poet wrote the famous poem 'The Erlking'?", "Johann Wolfgang von Goethe", "Friedrich Schiller", "Heinrich Heine", "Rainer Maria Rilke", "Bertolt Brecht", 1],
+    ["Which German football club plays its home games at the Signal Iduna Park?", "Borussia Dortmund", "Bayern Munich", "Schalke 04", "Bayer Leverkusen", "Hamburg SV", 1],
+    ["What was the currency of Germany before the Euro?", "Deutsche Mark", "Reichsmark", "Pfennig", "Euro", "Dinar", 1]
+]
+
+print ("Let's check your knowledge about Germany!")
+
+score = 0
+
+for i in range(len(questions)):
+    q = questions[i]
+    print(f"\nQuestion {i + 1}")
+    print(q[0])
+    print(f"1. {q[1]}    2. {q[2]}")
+    print(f"3. {q[3]}    4. {q[4]}")
+    print(f"5. {q[5]}")
+    
+    reply = int(input("Enter your answer (1-5): "))
+    if reply == q[6]:
+        score += 1
+        print(f"Correct! Score: {score}")
+    else:
+        print("Incorrect answer!")
+        break
+
+print(f"\nYour final score is: {score}")
+
+if score == 0:
+    print("Zero! Germany remains a mystery.")
+elif score <= 10:
+    print("You’ve got a toe in Germany.")
+elif score <= 20:
+    print("Some basics, keep going!")
+elif score <= 30:
+    print("You're warming up!")
+elif score <= 40:
+    print("Nice! A decent start.")
+elif score <= 50:
+    print("You're halfway to Berlin!")
+elif score <= 60:
+    print("Solid! You know quite a bit.")
+elif score <= 70:
+    print("Great! You're well-informed.")
+elif score <= 80:
+    print("So close, you're almost there!")
+elif score <= 90:
+    print("Impressive! Almost perfect.")
+else:
+    print("You crushed it! Willkommen in Deutschland!")
